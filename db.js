d469d1bc254e5def33feddee6eaabc6c8077f70f
SaiGane5 <128854913+SaiGane5@users.noreply.github.com>
2024-08-06 19:19:09 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,11 +3,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
 });
 
 pool.on('error', (err, client) => {
